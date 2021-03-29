c08414882bec5dd30ae8b2d8372e43bba68b3960
Nicolas Plaza <pacorabo2@hotmail.com>
2021-03-29 16:25:56 +0200

    (main/landing): Landing component and import on App.js

--- marvelquizz/src/components/App/index.js
+++ marvelquizz/src/components/App/index.js
@@ -1,4 +1,5 @@
-import Header from '../Header'
+import Header from '../Header';
+import Landing from '../Landing';
 import '../../App.css';
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div>
       <Header/>
+      <Landing/>
     </div>
   );
 }

c200017a73ee68c0b0f02bca459f8fd4170e3949
Nicolas Plaza <pacorabo2@hotmail.com>
2021-03-29 18:05:05 +0200

    (main/footer): add credits on footer

--- marvelquizz/src/components/Footer/index.js
+++ marvelquizz/src/components/Footer/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer>
+      <div className="footer-container">
+        <p>Projet réalisé par Nicolas PLAZA - 2020</p>
+        <p>Les icônes wolverine, iron-man, spyder-man et batman provienntent du site iconFinder.com</p>
+      </div>
+    </footer>
   )
 }
 

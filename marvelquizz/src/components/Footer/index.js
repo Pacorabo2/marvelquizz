4f5f62a619611545f87a64e3c9bb76980dded69c
Nicolas Plaza <pacorabo2@hotmail.com>
2021-04-04 20:40:52 +0200

    (main/landing): btn on landing

--- marvelquizz/src/components/Footer/index.js
+++ marvelquizz/src/components/Footer/index.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer>
       <div className="footer-container">
-        <p>Projet réalisé par Nicolas PLAZA - 2020</p>
+        <p>Projet réalisé par Nicolas PLAZA - 2020 - d'après un tuto de DonkeyGeek</p>
         <p>Les icônes wolverine, iron-man, spyder-man et batman provienntent du site iconFinder.com</p>
       </div>
     </footer>

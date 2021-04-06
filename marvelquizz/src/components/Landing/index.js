db077c5d1220e0fb02f30f1dfbc2836116bd6c3a
Nicolas Plaza <pacorabo2@hotmail.com>
2021-04-06 09:07:22 +0200

    (main/landing): state creation with conditional rendering of buttons and Fragment

--- marvelquizz/src/components/Landing/index.js
+++ marvelquizz/src/components/Landing/index.js
@@ -1,12 +1,33 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState, Fragment } from 'react';
 
 const Landing = () => {
 
-  const refWolverine = useRef(null);
+  const refWolverine = useRef(false);
+
+  const [btn, setBtn] = useState(null);
+
+  console.log(btn);
 
   useEffect(() => {
-    refWolverine.current.classList.add('leftClaws')
+    // Add ClassName to show left claws
+    refWolverine.current.classList.add('leftClaws');
+    setTimeout(() => {
+      // remove className tohide left claws
+      refWolverine.current.classList.remove('leftClaws')
+      setBtn(true)
+    }, 3000)
   }, [])
+
+  const displayBtn = btn && (
+    <Fragment>
+       <div className="leftBox">
+        <button className="btn-welcome">Inscription</button>
+      </div>
+      <div className="rightBox">
+        <button className="btn-welcome">Connexion</button>
+      </div>
+    </Fragment>
+  )
 
 
 
@@ -14,12 +35,7 @@ const Landing = () => {
 
   return (
     <main ref={refWolverine}className="welcomePage">
-      <div className="leftBox">
-        <button className="btn-welcome">Inscription</button>
-      </div>
-      <div className="rightBox">
-        <button className="btn-welcome">Connexion</button>
-      </div>
+      {displayBtn}
     </main>
   )
 }

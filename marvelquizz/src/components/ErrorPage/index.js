e8dd3a59a3200aab2c38b9a09a0e693096904eba
Nicolas Plaza <pacorabo2@hotmail.com>
2021-04-07 11:15:21 +0200

    (main/ErrorPage): change 404 img

--- marvelquizz/src/components/ErrorPage/index.js
+++ marvelquizz/src/components/ErrorPage/index.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Deadpool from '../../images/404orange.png'
 
 const ErrorPage = () => {
   return (
     <div className="quiz-bg">
-      <div className="cointainer">
-        ErrorPage
+      <div className="container">
+        <img src={ Deadpool } alt="error Page"/>
       </div>
     </div>
   )

ef828ee951ef61ac4455c83c6c890e6cf5704ede
Nicolas Plaza <pacorabo2@hotmail.com>
2021-04-07 21:32:08 +0200

    (main/SignUp): get state on change value of input

--- marvelquizz/src/components/SignUp/index.js
+++ marvelquizz/src/components/SignUp/index.js
@@ -14,8 +14,11 @@ const SignUp = () => {
   console.log(loginData);
 
   const handleChange = e => {
-
+    setLoginData({...loginData, [e.target.id]: e.target.value})
+    // console.log(e.target.id);
   }
+
+ 
 
   const { pseudo, email, password, confirmPassword } = loginData;
 
@@ -33,22 +36,22 @@ const SignUp = () => {
             <form action="">
             
               <div className="inputBox">
-                <input onchange={handleChange} value={pseudo} type="text" id="pseudo" autoComplete="off" required/>
+                <input onChange={handleChange} value={pseudo} type="text" id="pseudo" autoComplete="off" required/>
                 <label htmlFor="pseudo">Pseudo</label>
               </div>
 
               <div className="inputBox">
-                <input onchange={handleChange} value={email} type="email" id="email" autoComplete="off" required/>
+                <input onChange={handleChange} value={email} type="email" id="email" autoComplete="off" required/>
                 <label htmlFor="email">Email</label>
               </div>
 
               <div className="inputBox">
-                <input onchange={handleChange} value={password} type="password" id="password" autoComplete="off" required/>
+                <input onChange={handleChange} value={password} type="password" id="password" autoComplete="off" required/>
                 <label htmlFor="password">Mot de passe</label>
               </div>
 
               <div className="inputBox">
-                <input onchange={handleChange} value={confirmPassword} pe="password" id="confirmPassword" autoComplete="off" required/>
+                <input onChange={handleChange} value={confirmPassword} pe="password" id="confirmPassword" autoComplete="off" required/>
                 <label htmlFor="confirmPassword">Confirmer le mot de passe</label>
               </div>
             </form>

460d29b1e67ec66a4c42cacfeb22a2ce6be703a1
Nicolas Plaza <pacorabo2@hotmail.com>
2021-04-08 16:43:11 +0200

    (main/Welcome): check if user is authenticated and disconnect

--- marvelquizz/src/components/Welcome/index.js
+++ marvelquizz/src/components/Welcome/index.js
@@ -1,11 +1,27 @@
-import React, { useState, Fragment } from 'react'
+import React, { useState, Fragment, useContext, useEffect } from 'react'
+import { FirebaseContext } from '../Firebase'
 import Logout from '../Logout'
 import Quiz from '../Quiz'
 
-const Welcome = () => {
+const Welcome = props => {
 
   // Initialise userSession state
   const [userSession, setUserSession] = useState(null)
+
+  // Firebase context
+  const firebase = useContext(FirebaseContext)
+
+  useEffect(() => {
+    // To listen if there is a autenticated user
+    let listener = firebase.auth.onAuthStateChanged(user => {
+      // if not, redirect on '/'
+      user ? setUserSession(user) : props.history.push('/')
+    })
+    return () => {
+      // To cleanUp. stop the listener
+      listener()
+    }
+  }, [])
 
   return userSession === null ? (
     <Fragment>

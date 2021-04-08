af2d3ad02361146a970e67b1bb6c7530e34ea3da
Nicolas Plaza <pacorabo2@hotmail.com>
2021-04-08 14:30:10 +0200

    (main/Welcome): css of Lougout and Index

--- marvelquizz/src/components/Logout/index.js
+++ marvelquizz/src/components/Logout/index.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Logout = () => {
   return (
-    <div>
-      Logout
+    <div className="logoutContainer">
+      <label className="switch">
+        <input 
+          type="checkbox" 
+          name="" 
+          id=""
+          />
+          <span className="slider round"></span>
+      </label>
     </div>
   )
 }

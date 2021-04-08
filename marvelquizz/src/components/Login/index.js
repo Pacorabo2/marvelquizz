12b194c73ae2f9d8a5d97c0b81b199b98a90dc0d
Nicolas Plaza <pacorabo2@hotmail.com>
2021-04-08 10:15:35 +0200

    (main/login): login form shape

--- marvelquizz/src/components/Login/index.js
+++ marvelquizz/src/components/Login/index.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
     <div className="signUpLoginBox">
       <div className="slContainer">
-        Login
+        <div className="loginBoxLeftSignup">
+
+        </div>
+        <div className="loginBoxRight">
+          <div className="formContent">
+
+            <h2>Connexion</h2>
+
+            <form >
+              <div className="inputBox">
+                <input type="email" id="email" autoComplete="off" required/>
+                <label htmlFor="email">Email</label>
+              </div>
+
+              <div className="inputBox">
+                <input type="password" id="password" autoComplete="off" required/>
+                <label htmlFor="password">Mot de passe</label>
+              </div>
+            </form>
+            <div className="linkContainer">
+              <Link className="simpleLink" to="/signup">Nouveau sur Marvel Quiz ? Inscrivez-vous maintenant.</Link>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )

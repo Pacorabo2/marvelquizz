d64d2cbfadc64a709269d7cd7e7b5f04fb66f6f5
Nicolas Plaza <pacorabo2@hotmail.com>
2021-04-08 15:22:32 +0200

    (main/Welcome): rendering components if conected

--- marvelquizz/src/components/Welcome/index.js
+++ marvelquizz/src/components/Welcome/index.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState, Fragment } from 'react'
 import Logout from '../Logout'
 import Quiz from '../Quiz'
 
 const Welcome = () => {
-  return (
+
+  // Initialise userSession state
+  const [userSession, setUserSession] = useState(null)
+
+  return userSession === null ? (
+    <Fragment>
+      <div className="loader"></div>
+      <p className="loaderText">Loading ...</p>
+    </Fragment>
+  ) : (
     <div className="quiz-bg">
       <div className="container">
         <Logout/>

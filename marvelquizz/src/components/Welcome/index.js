77fe9d738c5a7ad56a4a7065f7412415a72f7682
Nicolas Plaza <pacorabo2@hotmail.com>
2021-04-08 14:06:20 +0200

    (main/Welcome): Logout import in Welcome component

--- marvelquizz/src/components/Welcome/index.js
+++ marvelquizz/src/components/Welcome/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Logout from '../Logout'
 
 const Welcome = () => {
   return (
     <div className="quiz-bg">
       <div className="container">
         Welcome
+        <Logout/>
       </div>
     </div>
   )

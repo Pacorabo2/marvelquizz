3241401ff49604692920edcc933082065eedccc2
Nicolas Plaza <pacorabo2@hotmail.com>
2021-04-09 15:24:01 +0200

    (main/Quiz): fixing className bugg on Quiz component

--- marvelquizz/src/components/Quiz/index.js
+++ marvelquizz/src/components/Quiz/index.js
@@ -13,10 +13,10 @@ class Quiz extends Component {
           <Levels/>
           <ProgressBar/>
           <h2>Notre question Quiz</h2>
-          <p className="answerOtions">Question 1</p>
-          <p className="answerOtions">Question 2</p>
-          <p className="answerOtions">Question 3</p>
-          <p className="answerOtions">Question 4</p>
+          <p className="answerOptions">Question 1</p>
+          <p className="answerOptions">Question 2</p>
+          <p className="answerOptions">Question 3</p>
+          <p className="answerOptions">Question 4</p>
           <button className="btnSubmit">Suivant</button>
         </div>
       )

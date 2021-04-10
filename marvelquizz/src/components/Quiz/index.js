b4fdef59fae661e57974321f9788f7ab097ced47
Nicolas Plaza <pacorabo2@hotmail.com>
2021-04-10 10:22:10 +0200

    (main/Quiz): enable button on selected {option}

--- marvelquizz/src/components/Quiz/index.js
+++ marvelquizz/src/components/Quiz/index.js
@@ -49,7 +49,8 @@ class Quiz extends Component {
 
   submitAnswer = selectedAnswer => {
     this.setState({
-      userAnswer: selectedAnswer
+      userAnswer: selectedAnswer,
+      btnDiasabled: false 
     })
   }
   

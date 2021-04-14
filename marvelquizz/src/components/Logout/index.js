428a397ac52d6a843a6d11341ff5d4d6d73d6720
Nicolas Plaza <pacorabo2@hotmail.com>
2021-04-14 11:39:33 +0200

    (main/Logout): define tooltip

--- marvelquizz/src/components/Logout/index.js
+++ marvelquizz/src/components/Logout/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { FirebaseContext } from '../Firebase'
+// Import tools
+import ReactToolTip from 'react-tooltip'
 
 
 const Logout = () => {
@@ -30,8 +32,9 @@ const Logout = () => {
           type="checkbox" 
           checked={checked} 
           />
-          <span className="slider round"></span>
+          <span className="slider round" data-tip="Déconnexion"></span>
       </label>
+      <ReactToolTip/>
     </div>
   )
 }

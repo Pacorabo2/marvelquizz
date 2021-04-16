4f58a55c056075987ce742435f5f7d0f59385629
Nicolas Plaza <pacorabo2@hotmail.com>
2021-04-16 17:18:43 +0200

    (main): Cleaning code and fix 404 displaying

--- marvelquizz/src/components/Footer/index.js
+++ marvelquizz/src/components/Footer/index.js
@@ -8,7 +8,6 @@ const Footer = () => {
     <footer>
       <div className="footer-container">
         <p>Projet réalisé par Nicolas PLAZA - 2020 - d'après un tuto de DonkeyGeek</p>
-        <p>Les icônes wolverine, iron-man, spyder-man et batman provienntent du site iconFinder.com</p>
       </div>
     </footer>
   )
